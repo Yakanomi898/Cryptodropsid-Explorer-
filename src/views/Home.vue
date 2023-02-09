d96e8ba85133603e36f251eeba94a8ec31693358
Cosmos.nodesdev <94297067+Yakanomi898@users.noreply.github.com>
2023-02-09 07:29:29 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          CRYPTODROPSID EXPLORER <small class="flow-left">.dev</small>
+          CRYPTODROPSID EXPLORER<small class="flow-left">.dev</small>
         </h1>
       </div>
     </b-link>
